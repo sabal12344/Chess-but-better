ab4acff50875e2d1c39fe4d05245970775311b10
Sabal Sigdel <sigdelsabal20@gmail.com>
2026-02-10 13:29:05 +0545

    img path update

--- chess/data/pieces.js
+++ chess/data/pieces.js
@@ -1,61 +1,62 @@
 export function blackpawn(current){
 
-    return{ current, image : "../chess/images/blackpawn.png", name : "BlackPawn", color : "black", type:"pawn"};
+    return{ current, image : "chess/images/blackpawn.png", name : "BlackPawn", color : "black", type:"pawn"};
 }
 
 export function blackrook(current){
-    return{ current, image : "../chess/images/blackrook.png", name : "BlackRook", color : "black"};
+    return{ current, image : "chess/images/blackrook.png", name : "BlackRook", color : "black"};
 
 }
 
 export function blackknight(current){
-    return{ current, image : "../chess/images/blackknight.png", name : "BlackKnight",color : "black"};
+    return{ current, image : "chess/images/blackknight.png", name : "BlackKnight",color : "black"};
 
 }
 
 export function blackqueen(current){
-    return{ current, image : "../chess/images/blackqueen.png", name : "BlackQueen",color : "black"};
+    return{ current, image : "chess/images/blackqueen.png", name : "BlackQueen",color : "black"};
 
 }
 
 export function blackking(current){
-    return{ current, image : "../chess/images/blackking.png", name : "BlackKing",color : "black"};
+    return{ current, image : "chess/images/blackking.png", name : "BlackKing",color : "black"};
 
 }
 
 export function blackbishop(current){
-    return{ current, image : "../chess/images/blackbishop.png", name : "BlackBishop",color : "black"};
+    return{ current, image : "chess/images/blackbishop.png", name : "BlackBishop",color : "black"};
 
 }
 export function whitepawn(current){
-    return{ current, image : "../chess/images/whitepawn.png", name : "WhitePawn", color : "white", type : "pawn"};
+    return{ current, image : "chess/images/whitepawn.png", name : "WhitePawn", color : "white", type : "pawn"};
 
 }
 
 export function whiterook(current){
-    return{ current, image : "../chess/images/whiterook.png", name : "WhiteRook", color : "white"};
+    return{ current, image : "chess/images/whiterook.png", name : "WhiteRook", color : "white"};
 
 }
 
 export function whiteknight(current){
-    return{ current, image : "../chess/images/whiteknight.png", name : "WhiteKnight", color : "white"};
+    return{ current, image : "chess/images/whiteknight.png", name : "WhiteKnight", color : "white"};
 
 }
 
 export function whitequeen(current){
-    return{ current, image : "../chess/images/whitequeen.png", name : "WhiteQueen", color : "white"};
+    return{ current, image : "chess/images/whitequeen.png", name : "WhiteQueen", color : "white"};
 
 }
 
 export function whiteking(current){
-    return{ current, image : "../chess/images/whiteking.png", name : "WhiteKing", color : "white"};
+    return{ current, image : "chess/images/whiteking.png", name : "WhiteKing", color : "white"};
 
 }
 
 export function whitebishop(current){
-    return{ current, image : "../chess/images/whitebishop.png", name : "WhiteBishop", color : "white"};
+    return{ current, image : "chess/images/whitebishop.png", name : "WhiteBishop", color : "white"};
 
 }
+
 
 
 
